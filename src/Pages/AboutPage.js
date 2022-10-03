65c3afc053463fcd69877518cd9e7d0060be9e29
chingyanglin <realchingyanglin@gmail.com>
2022-10-04 00:10:41 +0800

    2. About Section

---
+++ src/Pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+// Page Components
+import IntroSection from "../components/IntroSection";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <IntroSection />
+    </div>
+  );
+}
