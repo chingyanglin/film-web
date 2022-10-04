f8c95871007c389b8b25b249a78b388413f66279
chingyanglin <realchingyanglin@gmail.com>
2022-10-04 21:44:13 +0800

    Service Section

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 // Page Components
 import IntroSection from "../components/IntroSection";
+import Services from "../components/Services";
 
 export default function AboutPage() {
   return (
     <div>
       <IntroSection />
+      <Services />
     </div>
   );
 }
