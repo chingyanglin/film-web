c6c01b034050f4a80b1564746c1d728724e2febb
chingyanglin <realchingyanglin@gmail.com>
2022-10-04 21:51:16 +0800

    Faq Section

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -2,12 +2,13 @@ import React from "react";
 // Page Components
 import IntroSection from "../components/IntroSection";
 import Services from "../components/Services";
-
+import Faq from "../components/Faq";
 export default function AboutPage() {
   return (
     <div>
       <IntroSection />
       <Services />
+      <Faq />
     </div>
   );
 }
